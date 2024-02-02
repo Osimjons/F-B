f7ca326cf053281a2711bcce97d388e199616d59
Osimjons <74949726+Osimjons@users.noreply.github.com>
2024-02-02 18:07:10 +0300

    fixed bag  bg img

---
+++ script/index.js
@@ -0,0 +1,12 @@
+'use sctrict';
+// const menuItem = document.querySelectorAll('.list__iten');
+// addEventListener('click', () => {
+//   menuItem.classList.add('active');
+//   console.log('menuItem: ', menuItem);
+
+// });
+
+document.addEventListener('click', function(e){
+  const targetElement = e.target;
+  console.log('targetElement: ', targetElement);
+})
